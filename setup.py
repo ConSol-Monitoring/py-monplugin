484750c5c1df0ebbc48b5c5e570733e6d42fa20e
Danijel Tasov <data@consol.de>
2022-07-11 12:49:38 +0200

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="monplugin",
+    version="0.1",
+    description="Partial python port of perls Monitoring::Plugin",
+    author="Danijel Tasov",
+    author_email="m@rbfh.de",
+    packages=["monplugin"],
+)
